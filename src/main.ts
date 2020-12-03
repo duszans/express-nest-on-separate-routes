812ac0f9a608635c6806829d894d7fac8e17f6eb
Przemysław Duszyński <przemyslaw@dronedeploy.com>
2020-12-03 16:07:14 +0100

    Replaced express.Router() with just express()

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  const router = express.Router();
+  const router = express();
   router.get('/', (req, res) => {
     res.send('Hello from express!');
   });
   app.use('/express', router);
-
 
   await app.listen(3000);
 }
