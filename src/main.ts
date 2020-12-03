f0ff7ea7d10785792481996501c51674e4e857bc
Przemysław Duszyński <przemyslaw@dronedeploy.com>
2020-12-03 15:59:33 +0100

    nest app with express router

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as express from 'express';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const router = express.Router();
+  router.get('/', (req, res) => {
+    res.send('Hello from express!');
+  });
+  app.use('/express', router);
+
+
+  await app.listen(3000);
+}
+bootstrap();
